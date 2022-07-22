ae0264513208ce0cbe99b8b0f34ef1071e3d887c
Geovanni Correia <geovanni-dsc@hotmail.com>
2022-07-21 21:08:19 -0300

    Primeira Versão

---
+++ src/Program.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		
+		list.add(new Product ("TV", 900.00));
+		list.add(new Product ("Mouse", 80.00));
+		list.add(new Product ("Tablet", 1900.00));
+		list.add(new Product ("HD", 950.00));
+		
+		
+		//System.out.println(list[0]);
+		
+		
+		
+		
+		Consumer<Product> cons = p -> {
+			p.setValor(p.getValor()*1.1);
+		};
+		
+		list.forEach(cons);
+		
+		for(Product p : list) {
+			
+			System.out.println(p);
+			
+		}
+		
+		
+		
+	}
+
+}
